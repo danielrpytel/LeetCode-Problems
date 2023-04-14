55a9328e30db92e57d419fd74128dfabc9d0e28c
Daniel Pytel <danielrpytel@gmail.com>
2023-04-14 16:45:00 -0500

    First LeetCode Problem

---
+++ 1-TwoSum.js
@@ -0,0 +1,42 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Constraints:
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+const nums = [2,7,11,15];
+const target = 9;
+
+var twoSum = function(nums, target) {
+    const indices = new Map();
+
+    for (let index = 0; index < nums.length; index++) {
+        const complement = target - nums[index];
+
+        if (indices.has(complement)) {
+            return [indices.get(complement), index]
+        }
+
+        indices.set(nums[index], index)
+    }
+};
+
+console.log(twoSum(nums,target));
